033ea8a610597fda59c163e584912dc0e40cd96a
MattMC05 <matheusm.cogliatti@gmail.com>
2026-06-30 18:38:01 -0300

    Fix: Node Modules

--- backend/src/main/java/com/ecommerce/repository/PedidoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce.repository;
-
-import com.ecommerce.entity.Pedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PedidoRepository extends JpaRepository<Pedido, Long> {
-}
